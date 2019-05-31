4b9e3839b7ce8975d49040f2e7a8df64a0712048
Shailendra Sodhi <s.sodhi@gns.cri.nz>
2019-05-31 15:33:35 +1200

    Adding option to add stream log_format and access_logs.

--- manifests/init.pp
+++ manifests/init.pp
@@ -123,6 +123,7 @@ class nginx (
   $keepalive_timeout                                         = '65s',
   $keepalive_requests                                        = '100',
   Hash[String[1], Nginx::LogFormat] $log_format              = {},
+  $stream_log_format                                         = {},
   Boolean $mail                                              = false,
   Variant[String, Boolean] $mime_types_path                  = 'mime.types',
   Boolean $stream                                            = false,
